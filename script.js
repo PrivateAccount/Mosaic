db5c95e4d9acd2c33b792df6af337b708eadae29
Andrzej <andrzuk@gmail.com>
2024-02-22 12:05:51 +0100

    Additional step - grouping pieces in corner.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function() {
     var positions = [];
 
     const steps = 200;
+    const delay = 3000;
 
     const ROWS = 10;
     const COLUMNS = 19;
@@ -38,120 +39,129 @@ window.onload = function() {
 
     function getImagePiece(imageSrc, id, x, y, width, height, callback) {
         var img = new Image();  
-        img.src = imageSrc;
-        
+        img.src = imageSrc;        
         img.onload = function() {        
             var pieceDiv = document.createElement('div');
             pieceDiv.id = id;
             pieceDiv.classList.add('image-piece');
-
             pieceDiv.style.backgroundImage = 'url(' + imageSrc + ')';
             pieceDiv.style.backgroundPosition = '-' + x + 'px -' + y + 'px'; 
-
             pieceDiv.style.left = x + 'px';
             pieceDiv.style.top = y + 'px';
             pieceDiv.style.width = width + 'px';
             pieceDiv.style.height = height + 'px';
-        
             document.getElementById('imagePiece').appendChild(pieceDiv);
-
             callback();
         };
     }
 
     function getPiece(image, size, rows, cols, idx) {
         const x = size * Math.floor(idx % cols);
-        const y = size * Math.floor(idx / cols);
-        
+        const y = size * Math.floor(idx / cols);        
         getImagePiece(image, 'piece-' + idx, x, y, size, size, function() {
-            var alpha, x, y, method;
             if (idx < rows * cols - 1) {
                 getPiece(image, size, rows, cols, idx + 1);
             }
             else {
                 setTimeout(function() {
-                    method = Math.floor(Math.random() * 2) + 1;
                     for (var i = 0; i < ROWS * COLUMNS; i++) {
-                        if (method == 0) {
-                            alpha = 2 * Math.PI * i / (ROWS * COLUMNS);
-                            x = SCREEN_WIDTH / 2 - PIECE_SIZE / 2 + SCREEN_WIDTH / 4 * Math.cos(alpha);
-                            y = SCREEN_HEIGHT / 2 - PIECE_SIZE / 2 + SCREEN_HEIGHT / 4 * Math.sin(alpha);    
-                        }
-                        else {
-                            x = Math.floor(Math.random() * (SCREEN_WIDTH - PIECE_SIZE - 2 * BORDER)) + BORDER;
-                            y = Math.floor(Math.random() * (SCREEN_HEIGHT - PIECE_SIZE - 2 * BORDER)) + BORDER;
-                        }
+                        const x = Math.floor(Math.random() * (SCREEN_WIDTH - PIECE_SIZE - 2 * BORDER)) + BORDER;
+                        const y = Math.floor(Math.random() * (SCREEN_HEIGHT - PIECE_SIZE - 2 * BORDER)) + BORDER;
                         initPiece(i, x, y);
                     }
-                }, 2000);
-                setTimeout(function() {
-                    for (var i = 0; i < ROWS * COLUMNS; i++) {
-                        resetPiece(i);
-                    }
-                }, 6000);
-                setTimeout(function() {
-                    var parentElement = document.getElementById('imagePiece');
-                    while (parentElement.firstChild) {
-                        parentElement.removeChild(parentElement.firstChild);
-                    }
-                    idx = 0;
-                    slideId = slideId < slideNames.length - 1 ? slideId + 1 : 0;
-                    slideName = slideNames[slideId];
-                    getPiece(slideName, PIECE_SIZE, ROWS, COLUMNS, idx);
-                }, 10000);
-            }    
-        });        
+                }, delay);
+            }
+        });
     }
 
     function initPiece(idx, toX, toY) {
         const onePiece = document.getElementById('piece-' + idx.toString());
         const fromX = parseInt(onePiece.style.left);
         const fromY = parseInt(onePiece.style.top);
-        onePiece.style.zIndex = 1;
-        movePiece(onePiece, fromX, fromY, toX, toY, steps, 0);
+        onePiece.style.zIndex = idx;
+        movePiece(onePiece, fromX, fromY, toX, toY, steps, 0, function() {
+            if (idx == ROWS * COLUMNS - 1) {
+                var shuffled = [];
+                for (var i = 0; i < ROWS * COLUMNS; i++) {
+                    do {
+                        const randomIdx = Math.floor(Math.random() * ROWS * COLUMNS);
+                        if (shuffled.indexOf(randomIdx) === -1) {
+                            shuffled.push(randomIdx);
+                        }
+                    }
+                    while (shuffled.length < ROWS * COLUMNS);
+                }
+                idx = 0;
+                setTimeout(function() {
+                    groupToCorner(idx, shuffled);
+                }, delay);
+            }
+        });
         positions[idx] = { fromX: fromX, fromY: fromY, toX: toX, toY: toY };
     }
 
-    function movePiece(element, fromX, fromY, toX, toY, steps, step) {
+    function movePiece(element, fromX, fromY, toX, toY, steps, step, callback) {
+        const dt = 10;
         const posX = parseInt(element.style.left);
         const posY = parseInt(element.style.top);
         const dx = (toX - posX) / (steps - step);
         const dy = (toY - posY) / (steps - step);
-
         element.style.left = (posX + dx).toString() + 'px';
         element.style.top = (posY + dy).toString() + 'px';
-
         if (step < steps) {
             setTimeout(function() {
-                movePiece(element, fromX, fromY, toX, toY, steps, step + 1);
-            }, 10);
+                movePiece(element, fromX, fromY, toX, toY, steps, step + 1, callback);
+            }, dt);
+        }
+        else {
+            callback();
         }
     }
 
-    function backPiece(element, fromX, fromY, toX, toY, steps, step) {
-        const posX = parseInt(element.style.left);
-        const posY = parseInt(element.style.top);
-        const dx = (toX - posX) / (steps - step);
-        const dy = (toY - posY) / (steps - step);
-
-        element.style.left = (posX + dx).toString() + 'px';
-        element.style.top = (posY + dy).toString() + 'px';
-
-        if (step < steps) {
+    function groupToCorner(idx, items) {
+        if (idx < items.length) {
+            const onePiece = document.getElementById('piece-' + items[idx].toString());
+            const fromX = parseInt(onePiece.style.left);
+            const fromY = parseInt(onePiece.style.top);
+            const toX = SCREEN_WIDTH - PIECE_SIZE - 2 * idx - 2 * BORDER;
+            const toY = SCREEN_HEIGHT - PIECE_SIZE - 2 * idx - 2 * BORDER;
+            onePiece.style.zIndex = items.length - idx;
+            movePiece(onePiece, fromX, fromY, toX, toY, steps / 10, 0, function() {
+                groupToCorner(idx + 1, items);
+            });
+        }
+        else {
+            idx = 0;
             setTimeout(function() {
-                backPiece(element, fromX, fromY, toX, toY, steps, step + 1);
-            }, 10);
+                restorePicture(idx, items);
+            }, delay);
         }
     }
 
-    function resetPiece(idx) {
-        const onePiece = document.getElementById('piece-' + idx.toString());
-        onePiece.style.zIndex = 2;
-        const fromX = positions[idx].toX;
-        const fromY = positions[idx].toY;
-        const toX = positions[idx].fromX;
-        const toY = positions[idx].fromY;
-        backPiece(onePiece, fromX, fromY, toX, toY, steps, 0);
+    function restorePicture(idx, items) {
+        if (idx < items.length) {
+            const onePiece = document.getElementById('piece-' + items[idx].toString());
+            const fromX = parseInt(onePiece.style.left);
+            const fromY = parseInt(onePiece.style.top);
+            const toX = positions[items[idx]].fromX;
+            const toY = positions[items[idx]].fromY;
+            onePiece.style.zIndex = items.length + idx;
+            movePiece(onePiece, fromX, fromY, toX, toY, steps / 10, 0, function() {
+                restorePicture(idx + 1, items);
+            });
+        }
+        else {
+            setTimeout(function() {
+                var parentElement = document.getElementById('imagePiece');
+                while (parentElement.firstChild) {
+                    parentElement.removeChild(parentElement.firstChild);
+                }
+                idx = 0;
+                slideId = slideId < slideNames.length - 1 ? slideId + 1 : 0;
+                slideName = slideNames[slideId];
+                getPiece(slideName, PIECE_SIZE, ROWS, COLUMNS, idx);
+            }, delay);
+        }
     }
 
 };
